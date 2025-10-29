16d351ea5d5b6e22281f284b9f73aa3b35934113
Dhruv <dhruvranap4521@gmail.com>
2025-10-30 00:38:40 +0530

    Update 09_reverse_the_array.c

--- 04 Using Arrays/09_reverse_the_array.c
+++ 04 Using Arrays/09_reverse_the_array.c
@@ -23,3 +23,36 @@ int main() {
 
     return 0;
 }
+
+//  2nd method
+
+int reverse(){
+
+    int a[5], n, i;
+
+    printf("Enter number of elements (max 5): ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("\nOriginal array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    
+    for(i=0;i<n/2;i++){
+        a[n-i-1]+=a[i];
+        a[i]=a[n-i-1]-a[i];
+        a[n-1-i]=a[n-i-1]-a[i];
+    }
+
+    printf("\nReversed array: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d ", a[i]);
+    }
+
+}
+
